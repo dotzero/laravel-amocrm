8f4a0c0a32fa4b491a1032e35be5fc79af6f5855
dotzero <mail@dotzero.ru>
2016-11-13 21:32:12 +0300

    Update Facade

--- src/Facades/AmoCrm.php
+++ src/Facades/AmoCrm.php
@@ -1,10 +1,25 @@
 <?php
 
+/*
+ * This file is part of Laravel AmoCrm.
+ *
+ * (c) dotzero <mail@dotzero.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Dotzero\LaravelAmoCrm\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * This is the AmoCrm facade class.
+ *
+ * @package Dotzero\LaravelAmoCrm\Facades
+ * @author dotzero <mail@dotzero.ru>
+ * @link http://www.dotzero.ru/
+ * @link https://github.com/dotzero/laravel-amocrm
  * @see \Dotzero\LaravelAmoCrm\AmoCrmServiceProvider
  */
 class AmoCrm extends Facade
